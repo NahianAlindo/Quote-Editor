0c64762a7c0b737c5c55ff13c1c3e714e7dcc4a8
Nahian Rifaat <nrifaat26@gmail.com>
2023-01-08 18:24:41 +0600

    empty state handled using turbo stream.

--- app/javascript/controllers/removals_controller.js
+++ app/javascript/controllers/removals_controller.js
@@ -2,8 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="removals"
 export default class extends Controller {
-  connect() {
-  }
   remove(){
     this.element.remove()
   }
